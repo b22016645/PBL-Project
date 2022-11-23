a7e4e7e1f7dc914f3c65887fc396d212ffd75775
b22016645 <byeolstar12@naver.com>
2022-11-23 16:10:04 +0900

    세팅페이지

--- app/src/main/java/com/example/pbl_project/SettingActivity.kt
+++ app/src/main/java/com/example/pbl_project/SettingActivity.kt
@@ -1,6 +1,23 @@
 package com.example.pbl_project
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.pbl_project.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySettingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.donebtn.setOnClickListener {
+            //마이페이지로 돌아가기
+        }
+
+
+
+    }
 }
